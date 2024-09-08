6107c2e628b689d7c3dbc2beed2c879cc7f32268
Laura Comar <lautmcomar@gmail.com>
2024-09-08 14:49:50 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -4,3 +4,16 @@ function calculateAverageSales(sales) {
     let total= sales.reduce((sum, sales) => sum + sales, 0);
     return total / salesfigures.length; 
 }
+
+// Task 2: Create a Function to Determine Performance Rating
+function  determinePerformanceRating(avgsales){
+    if (avgsales > 10000)
+        console.log(`Excellent`);
+    else if (avgsales>= 7000 && avgsales<= 10000)
+        console.log (`Good`);
+    else if (avgsales>=4000 && avgsales<7000)
+        console.log (`Satisfactory`);
+    else (avgsales<4000)
+    console.log (`Needs Improvement`);
+}
+
