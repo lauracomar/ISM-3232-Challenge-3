379280826b9a0bce3a965ff84dfba3771523e374
Laura Comar <lautmcomar@gmail.com>
2024-09-08 21:11:39 -0400

    Develop findTopAndBottomPerformers function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,8 +1,8 @@
 //Task 1: Create a Function to Calculate Average Sales
-function calculateAverageSales(sales) {
-    if (salesfigures.length === 0) return 0; // empty array
-    let total= sales.reduce((sum, sales) => sum + sales, 0);
-    return total / salesfigures.length; 
+function calculateAverageSales(salesFigures) {
+    if (salesFigures.length === 0) return 0; // empty array
+    let total= salesFigures.reduce((sum, sales) => sum + sales, 0);
+    return total / salesFigures.length; 
 }
 
 // Task 2: Create a Function to Determine Performance Rating
@@ -13,7 +13,18 @@ function  determinePerformanceRating(avgsales){
         console.log (`Good`);
     else if (avgsales>=4000 && avgsales<7000)
         console.log (`Satisfactory`);
-    else (avgsales<4000)
-    console.log (`Needs Improvement`);
+    else console.log(`Needs Improvement`);
+}
+
+// Task 3: Create a Function to Identify Top and Bottom Performers
+function findTopAndBottomPerformers(data){
+    if (data.length === 0 ) return {topPerformer: null, bottomPerformer: null}
+
+    const topPerformer = data.reduce((top, salesPerson) => calculateAverageSales(top.sales) >= calculateAverageSales(salesPerson.sales) ? top : salesPerson  ,data[0])
+
+    const bottomPerformer = data.reduce((bottom, salesPerson) => calculateAverageSales(bottom.sales) <= calculateAverageSales(salesPerson.sales) ? bottom : salesPerson  ,data[0])
+    
+    return {topPerformer, bottomPerformer}
+    
 }
 
